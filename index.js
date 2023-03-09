c87268afa30ceddc6c961bf4cc46e7633a54599a
Nick Currie <gosox997@gmail.com>
2023-03-08 23:18:49 -0500

    passed all tests

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a, b) {
+    return a + b;
+
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function increment(n) {
+    let number;
+    number = n + 1
+    return number
+}
+
+function decrement(n) {
+    let number;
+    number = n - 1
+    return number
+}
+
+function makeInt(n) {
+    let number
+    number = parseInt(n, 10);
+    return number
+}
+
+function preserveDecimal(n) {
+    let number
+    number = parseFloat(n, 10);
+    return number
+}
